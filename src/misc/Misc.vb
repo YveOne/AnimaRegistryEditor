8950b70613874dd4f5d4c9abf9d2657bd33f672a
Yvonne P <contact@yveone.com>
2018-12-21 23:49:01 +0100

    Update to v1.2

---
+++ src/misc/Misc.vb
@@ -0,0 +1,12 @@
+﻿Module Misc
+
+    Public Function CIntSave(val As Object) As Integer
+        Try
+            Return CInt(val)
+        Catch ex As Exception
+            MessageBox.Show("Cannot cast """ & CStr(val) & """ to int")
+            Return 0
+        End Try
+    End Function
+
+End Module
